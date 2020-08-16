99ca2ca076e68fb4733bb7535899a8d4bf2b62cd
Michael <ivanyushchenkov@gemark.ru>
2020-08-16 20:18:47 +0300

    first commit

---
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/RootActivity.kt
@@ -0,0 +1,128 @@
+package ru.skillbranch.skillarticles.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_root.*
+import kotlinx.android.synthetic.main.layout_bottombar.*
+import kotlinx.android.synthetic.main.layout_submenu.*
+import ru.skillbranch.skillarticles.R
+import ru.skillbranch.skillarticles.extensions.dpToIntPx
+import ru.skillbranch.skillarticles.viewmodels.ArticleState
+import ru.skillbranch.skillarticles.viewmodels.ArticleViewModel
+import ru.skillbranch.skillarticles.viewmodels.Notify
+import ru.skillbranch.skillarticles.viewmodels.ViewModelFactory
+
+class RootActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: ArticleViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_root)
+        setupToolbar()
+        setupBottomBar()
+        setupSubMenu()
+
+        val vmFactory = ViewModelFactory("0")
+        viewModel = ViewModelProviders.of(this, vmFactory).get(ArticleViewModel::class.java)
+        viewModel.observeState(this) {
+            renderUI(it)
+        }
+        viewModel.observeNotifications(this) {
+            renderNotification(it)
+        }
+    }
+
+    private fun renderUI(data: ArticleState) {
+        btn_settings.isChecked = data.isShowMenu
+        if (data.isShowMenu) subMenu.open() else subMenu.close()
+
+        btn_like.isChecked = data.isLike
+        btn_bookmark.isChecked = data.isBookmark
+
+        switch_mode.isChecked = data.isDarkMode
+        delegate.localNightMode = if (data.isDarkMode) {
+            AppCompatDelegate.MODE_NIGHT_YES
+        } else {
+            AppCompatDelegate.MODE_NIGHT_NO
+        }
+
+        if (data.isBigText) {
+            tvTextContent.textSize = 18f
+            btn_text_up.isChecked = true
+            btn_text_down.isChecked = false
+        } else {
+            tvTextContent.textSize = 14f
+            btn_text_up.isChecked = false
+            btn_text_down.isChecked = true
+        }
+
+        tvTextContent.text = if (data.isLoadingContent) "loading" else data.content.first() as String
+
+        toolbar.title = data.category ?: "loading"
+        toolbar.subtitle = data.category ?: "loading"
+        if (data.categoryIcon != null) toolbar.logo = getDrawable(data.categoryIcon as Int)
+    }
+
+    private fun renderNotification(notify: Notify) {
+        val snackbar = Snackbar.make(coordinatorContainer, notify.message, Snackbar.LENGTH_LONG)
+            .setAnchorView(bottomBar)
+            .setActionTextColor(ContextCompat.getColor(applicationContext, R.color.color_accent_dark))
+
+        when (notify) {
+            is Notify.TextMessage -> {}
+            is Notify.ActionMessage -> {
+                with(snackbar) {
+                    setBackgroundTint(ContextCompat.getColor(applicationContext, R.color.design_default_color_error))
+                    setTextColor(ContextCompat.getColor(applicationContext, android.R.color.white))
+                    setActionTextColor(ContextCompat.getColor(applicationContext, android.R.color.white))
+                    setAction(notify.actionLabel) {
+                        notify.actionHandler?.invoke()
+                    }
+                }
+            }
+            is Notify.ErrorMessage -> {
+                snackbar.setAction(notify.errorLabel) {
+                    notify.errorHandler?.invoke()
+                }
+            }
+        }
+
+        snackbar.show()
+    }
+
+    private fun setupSubMenu() {
+        btn_text_up.setOnClickListener { viewModel.handleUpText() }
+        btn_text_down.setOnClickListener { viewModel.handleDownText() }
+        switch_mode.setOnClickListener { viewModel.handleNightMode() }
+    }
+
+    private fun setupBottomBar() {
+        btn_like.setOnClickListener { viewModel.handleLike() }
+        btn_bookmark.setOnClickListener { viewModel.handleBookmark() }
+        btn_share.setOnClickListener { viewModel.handleShare() }
+        btn_settings.setOnClickListener { viewModel.handleToggleMenu() }
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val logo = if (toolbar.childCount > 2) toolbar.getChildAt(2) as ImageView else null
+        logo?.scaleType = ImageView.ScaleType.CENTER_CROP
+        val lp = logo?.layoutParams as? Toolbar.LayoutParams
+        lp?.let {
+            it.width = this.dpToIntPx(40)
+            it.height = this.dpToIntPx(40)
+            it.marginEnd = this.dpToIntPx(16)
+            logo.layoutParams = it
+        }
+    }
+}
